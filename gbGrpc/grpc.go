f43d37414db36a0f032e07034881d37b982ea9f3
Dinh <nqdinhddt@gmail.com>
2021-07-03 16:37:18 +0700

    record stream send receive

--- gbGrpc/grpc.go
+++ gbGrpc/grpc.go
@@ -80,16 +80,16 @@ func (cc *GbClientConn) setupMethod(method string) ([]metrics.Graph, error) {
 }
 
 func (cc *GbClientConn) Invoke(ctx context.Context, method string, args interface{}, reply interface{}, opts ...grpc.CallOption) error {
-	graphs, _ := cc.setupMethod(method)
+	graphs, err := cc.setupMethod(method)
+	if err != nil {
+		return err
+	}
 
 	begin := time.Now()
-
-	err := cc.ClientConn.Invoke(ctx, method, args, reply, opts...)
-
+	err = cc.ClientConn.Invoke(ctx, method, args, reply, opts...)
 	diff := time.Since(begin)
 
 	latencyTitle := graphs[1].Metrics[0].Title
-
 	countTitle := graphs[0].Metrics[0].Title
 	if err != nil {
 		countTitle = graphs[0].Metrics[1].Title
@@ -103,18 +103,14 @@ func (cc *GbClientConn) Invoke(ctx context.Context, method string, args interfac
 
 type GbClientStream struct {
 	grpc.ClientStream
-	methodGraphsMap map[string][]metrics.Graph
-	target          string
-	method          string
+	target string
+	method string
+	graphs []metrics.Graph
 }
 
 func (cs *GbClientStream) setupMethod(target string, method string) (
 	[]metrics.Graph, error,
 ) {
-	if graphs, ok := cs.methodGraphsMap[method]; ok {
-		return graphs, nil
-	}
-
 	graphs := []metrics.Graph{
 		{
 			Title: "New Stream",
@@ -131,18 +127,64 @@ func (cs *GbClientStream) setupMethod(target string, method string) (
 			},
 		},
 		{
-			Title: "Latency",
+			Title: "New Stream Latency",
 			Unit:  "Microsecond",
 			Metrics: []metrics.Metric{
 				{
-					Title: method + ".latency", // latency
+					Title: method + ".new_stream_latency", // latency
+					Type:  metrics.Histogram,
+				},
+			},
+		},
+		{
+			Title: "Send Message",
+			Unit:  "N",
+			Metrics: []metrics.Metric{
+				{
+					Title: method + ".send_msg_ok", // success
+					Type:  metrics.Counter,
+				},
+				{
+					Title: method + ".send_msg_fail", // fail
+					Type:  metrics.Counter,
+				},
+			},
+		},
+		{
+			Title: "Send Message Latency",
+			Unit:  "Microsecond",
+			Metrics: []metrics.Metric{
+				{
+					Title: method + ".send_msg_latency", // latency
+					Type:  metrics.Histogram,
+				},
+			},
+		},
+		{
+			Title: "Receive Message",
+			Unit:  "N",
+			Metrics: []metrics.Metric{
+				{
+					Title: method + ".recv_msg_ok", // success
+					Type:  metrics.Counter,
+				},
+				{
+					Title: method + ".recv_msg_fail", // fail
+					Type:  metrics.Counter,
+				},
+			},
+		},
+		{
+			Title: "Receive Message Latency",
+			Unit:  "Microsecond",
+			Metrics: []metrics.Metric{
+				{
+					Title: method + ".recv_msg_latency", // latency
 					Type:  metrics.Histogram,
 				},
 			},
 		},
 	}
-
-	cs.methodGraphsMap[method] = graphs
 
 	group := metrics.Group{
 		Name:   "gRPC stream (" + cs.target + ")",
@@ -153,38 +195,74 @@ func (cs *GbClientStream) setupMethod(target string, method string) (
 		group,
 	}
 
+	cs.graphs = graphs
+
 	err := executor.Setup(groups)
 
 	return graphs, err
 }
 
-/**
-what to record
-CloseSend
-SendMsg
-RecvMsg
-*/
 func (cc *GbClientConn) NewStream(ctx context.Context, desc *grpc.StreamDesc, method string, opts ...grpc.CallOption) (
 	grpc.ClientStream, error,
 ) {
 	gcn := &GbClientStream{
-		methodGraphsMap: make(map[string][]metrics.Graph),
-		target:          cc.target,
-		method:          method,
+		target: cc.target,
+		method: method,
 	}
 
-	graphs, _ := gcn.setupMethod(cc.target, method)
+	var err error
+
+	graphs, err := gcn.setupMethod(cc.target, method)
+	if err != nil {
+		return gcn, err
+	}
 
 	begin := time.Now()
-
-	cs, err := cc.ClientConn.NewStream(ctx, desc, method, opts...)
-	if err != nil {
-		return cs, err
-	}
-
+	gcn.ClientStream, err = cc.ClientConn.NewStream(ctx, desc, method, opts...)
 	diff := time.Since(begin)
 
-	gcn.ClientStream = cs
+	latencyTitle := graphs[1].Metrics[0].Title
+	countTitle := graphs[0].Metrics[0].Title
+	if err != nil {
+		countTitle = graphs[0].Metrics[1].Title
+	}
 
-	return cs, err
+	executor.Notify(latencyTitle, diff.Microseconds())
+	executor.Notify(countTitle, 1)
+
+	return gcn, err
+}
+
+func (cs *GbClientStream) SendMsg(m interface{}) error {
+	begin := time.Now()
+	err := cs.ClientStream.SendMsg(m)
+	diff := time.Since(begin)
+
+	latencyTitle := cs.graphs[3].Metrics[0].Title
+	countTitle := cs.graphs[2].Metrics[0].Title
+	if err != nil {
+		countTitle = cs.graphs[2].Metrics[1].Title
+	}
+
+	executor.Notify(latencyTitle, diff.Microseconds())
+	executor.Notify(countTitle, 1)
+
+	return err
+}
+
+func (cs *GbClientStream) RecvMsg(m interface{}) error {
+	begin := time.Now()
+	err := cs.ClientStream.RecvMsg(m)
+	diff := time.Since(begin)
+
+	latencyTitle := cs.graphs[5].Metrics[0].Title
+	countTitle := cs.graphs[4].Metrics[0].Title
+	if err != nil {
+		countTitle = cs.graphs[4].Metrics[1].Title
+	}
+
+	executor.Notify(latencyTitle, diff.Microseconds())
+	executor.Notify(countTitle, 1)
+
+	return err
 }
